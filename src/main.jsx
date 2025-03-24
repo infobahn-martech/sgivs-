a7001e9a5678957a5f75ce4937d4c442f83e69fb
ba-ba-yaga <SHANID@SPERICORN.COM>
2025-03-24 12:20:42 +0530

    changed index.css file path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
+import './assets/index.css';
 
 import router from './routes/index.jsx';
 
