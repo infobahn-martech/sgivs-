0935a36eddd6c9f0be7cf3c0f24fdddac641bd32
ba-ba-yaga <SHANID@SPERICORN.COM>
2025-03-25 13:36:05 +0530

    added footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="ftr">
+                <p className="copy">Dating Resources Network@2025</p>
+            </footer>
+  )
+}
+
+export default Footer
