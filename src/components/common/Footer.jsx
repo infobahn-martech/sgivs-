bc38a31bc4f6d52b8aa4003f4e5fd72d1ddde437
ba-ba-yaga <SHANID@SPERICORN.COM>
2025-03-25 14:21:24 +0530

    fixed sidebar design issue

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import '../../assets/scss/footer.scss';
 
 const Footer = () => {
   return (
     <footer className="ftr">
-                <p className="copy">Dating Resources Network@2025</p>
-            </footer>
-  )
-}
+      <p className="copy">Dating Resources Network@2025</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
