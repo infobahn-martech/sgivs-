b05035e85128bebb7ed63681352724b13a07a679
ba-ba-yaga <SHANID@SPERICORN.COM>
2025-03-25 13:11:25 +0530

    added routes

---
+++ src/pages/InventoryManagement/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InventoryManagement = () => {
+  return (
+    <div>Inventory Management</div>
+  )
+}
+
+export default InventoryManagement
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
-import { createBrowserRouter, Route } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import App from '../App';
-import Page404 from '../components/common/Page404';
-
 import PublicRoutes from './PublicRoutes';
 import PrivateRoutes from './PrivateRoutes';
+
+import Page404 from '../components/common/Page404';
 import Layout from '../components/common/Layout';
 import Login from '../pages/Auth';
+import InventoryManagement from '../pages/InventoryManagement/Loader';
 
 const router = createBrowserRouter([
   {
@@ -16,7 +17,6 @@ const router = createBrowserRouter([
       {
         element: <PublicRoutes />,
         children: [
-         
           {
             path: '/login',
             element: <Login />,
@@ -34,8 +34,28 @@ const router = createBrowserRouter([
                 element: <h1>Dashboard page</h1>,
               },
               {
-                path: '/profile',
-                element: <h1>Profile page</h1>,
+                element: <h1>User Management</h1>,
+                path: '/user-management',
+              },
+              {
+                element:<InventoryManagement />,
+                path: '/inventory-management',
+              },
+              {
+                element: <h1>Rental Management'</h1>,
+                path: '/rental-management',
+              },
+              {
+                element: <h1>EZ pass billing'</h1>,
+                path: '/ez-pass-billing',
+              },
+              {
+                element: <h1>Messages'</h1>,
+                path: '/messages',
+              },
+              {
+                element: <h1>Settings'</h1>,
+                path: '/settings',
               },
             ],
           },
