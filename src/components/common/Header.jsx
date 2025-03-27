0dd2933651a13ad6d081f81c32a959f8f86f01da
thechrister <dany@spericorn.com>
2025-03-27 09:39:59 +0530

    logout modal

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../assets/scss/header.scss';
 import '../../assets/scss/common.scss';
@@ -12,72 +12,96 @@ import NotificationIcon from '../../assets/images/notifications.svg';
 import UserIcon from '../../assets/images/avatar.png';
 import useAuthReducer from '../../stores/AuthReducer';
 import { useNavigate } from 'react-router-dom';
+import CustomActionModal from '../CustomActionModal';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [isLogOut, setIsLogOut] = useState(false);
   const { doLogout } = useAuthReducer((state) => state);
   const { profileData } = useAuthReducer((state) => state);
 
   return (
-    <div className="header">
-      <div className="header-inner">
-        <div className="user-details">
-          <div className="notification">
-            <span className="notification-icon">
-              <img src={NotificationIcon} alt="notification" />
-            </span>
-            <span className="no-of-notification">0</span>
-          </div>
-          <div className="dropdown user-drop">
-            <a
-              className="dropdown-toggle user-name"
-              href="#"
-              role="button"
-              id="userDropdown"
-              data-bs-toggle="dropdown"
-            >
-              <div className="usr-dtl">
-                <div className="user-image">
-                  <img src={UserIcon} alt="user" />
+    <>
+      <div className="header">
+        <div className="header-inner">
+          <div className="user-details">
+            <div className="notification">
+              <span className="notification-icon">
+                <img src={NotificationIcon} alt="notification" />
+              </span>
+              <span className="no-of-notification">0</span>
+            </div>
+            <div className="dropdown user-drop">
+              <a
+                className="dropdown-toggle user-name"
+                href="#"
+                role="button"
+                id="userDropdown"
+                data-bs-toggle="dropdown"
+              >
+                <div className="usr-dtl">
+                  <div className="user-image">
+                    <img src={UserIcon} alt="user" />
+                  </div>
+                  <div className="usr-info">
+                    <span className="name">
+                      {profileData?.employee?.firstName}{' '}
+                      {profileData?.employee?.lastName}
+                    </span>
+                    <span className="role">Administrator</span>
+                  </div>
                 </div>
-                <div className="usr-info">
-                  <span className="name">
-                    {profileData?.employee?.firstName}{' '}
-                    {profileData?.employee?.lastName}
-                  </span>
-                  <span className="role">Administrator</span>
-                </div>
-              </div>
-            </a>
-            <ul
-              className="dropdown-menu dropdown-menu-end"
-              aria-labelledby="userDropdown"
-            >
-              <li>
-                <button
-                  className="dropdown-item"
-                  onClick={() => {
-                    navigate('/profile');
-                  }}
-                >
-                  Profile
-                </button>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Settings
-                </a>
-              </li>
-              <li>
-                <button className="dropdown-item" onClick={() => doLogout()}>
-                  Logout
-                </button>
-              </li>
-            </ul>
+              </a>
+              <ul
+                className="dropdown-menu dropdown-menu-end"
+                aria-labelledby="userDropdown"
+              >
+                <li>
+                  <button
+                    type="button"
+                    className="dropdown-item"
+                    onClick={() => {
+                      navigate('/profile');
+                    }}
+                  >
+                    Profile
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="dropdown-item"
+                    onClick={() => {
+                      navigate('/settings');
+                    }}
+                  >
+                    Settings
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => setIsLogOut(true)}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {isLogOut && (
+        <CustomActionModal
+          showModal={isLogOut}
+          closeModal={() => {
+            setIsLogOut(false);
+          }}
+          message="Do you want to Logout?"
+          onSubmit={() => doLogout()}
+        />
+      )}
+    </>
   );
 };
 
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -11,85 +11,132 @@ import logOut from '../../assets/images/log-out.svg';
 import MyAccount from './MyAccount';
 import ChangePassword from './ChangePassword';
 import useAuthReducer from '../../stores/AuthReducer';
+import CustomActionModal from '../../components/CustomActionModal';
 
 const Profile = () => {
-  const [isMyAccount, setIsMyAccount] = useState(true);
-  const { profileData } = useAuthReducer((state) => state);
+  const [tab, setTab] = useState({
+    profile: true,
+    changePassword: false,
+    logout: false,
+  });
+  const [isLogOut, setIsLogOut] = useState(false);
+  const { profileData, doLogout } = useAuthReducer((state) => state);
 
   return (
-    <div className="profile-sec">
-      <div className="profile-title">
-        <span className="pro-pic">
-          <img src={proPic} alt="pro-pic" />
-        </span>
-        <span>Profile</span>
-      </div>
-      <div className="profile-inner">
-        <div className="profile-picture">
-          <div className="img-sec">
-            <img src={proPict} alt="profile-picture" />
-          </div>
-          <div className="u-name">
-            {' '}
-            {profileData?.employee?.firstName} {profileData?.employee?.lastName}
-          </div>
+    <>
+      <div className="profile-sec">
+        <div className="profile-title">
+          <span className="pro-pic">
+            <img src={proPic} alt="pro-pic" />
+          </span>
+          <span>Profile</span>
         </div>
+        <div className="profile-inner">
+          <div className="profile-picture">
+            <div className="img-sec">
+              <img src={proPict} alt="profile-picture" />
+            </div>
+            <div className="u-name">
+              {' '}
+              {profileData?.employee?.firstName}{' '}
+              {profileData?.employee?.lastName}
+            </div>
+          </div>
 
-        <div className="profile-tab">
-          <div className="profile-tab-inner">
-            <ul className="nav nav-tabs" id="profileTabs" role="tablist">
-              <li className="nav-item" role="presentation">
-                <button
-                  className={`nav-link ${isMyAccount ? 'active' : ''}`}
-                  type="button"
-                  onClick={() => setIsMyAccount(true)}
-                >
-                  <span>
-                    <img src={proPic} alt="profile-icon" />
-                  </span>
-                  Profile
-                </button>
-              </li>
+          <div className="profile-tab">
+            <div className="profile-tab-inner">
+              <ul className="nav nav-tabs" id="profileTabs" role="tablist">
+                <li className="nav-item" role="presentation">
+                  <button
+                    className={`nav-link ${tab?.profile ? 'active' : ''}`}
+                    type="button"
+                    onClick={() =>
+                      setTab({
+                        profile: true,
+                        changePassword: false,
+                        logout: false,
+                      })
+                    }
+                  >
+                    <span>
+                      <img src={proPic} alt="profile-icon" />
+                    </span>
+                    Profile
+                  </button>
+                </li>
 
-              <li className="nav-item" role="presentation">
-                <button
-                  className={`nav-link ${!isMyAccount ? 'active' : ''}`}
-                  type="button"
-                  onClick={() => setIsMyAccount(false)}
-                >
-                  <span>
-                    <img src={lock} alt="lock" />
-                  </span>
-                  Change Password
-                </button>
-              </li>
-              <li className="nav-item" role="presentation">
-                <a
-                  className="nav-link"
-                  href="#"
-                  aria-selected="false"
-                  tabIndex="-1"
-                  role="tab"
-                >
-                  <span>
-                    <img src={logOut} alt="logout-icon" />
-                  </span>
-                  Logout
-                </a>
-              </li>
-            </ul>
+                <li className="nav-item" role="presentation">
+                  <button
+                    className={`nav-link ${
+                      tab?.changePassword ? 'active' : ''
+                    }`}
+                    type="button"
+                    onClick={() =>
+                      setTab({
+                        profile: false,
+                        changePassword: true,
+                        logout: false,
+                      })
+                    }
+                  >
+                    <span>
+                      <img src={lock} alt="lock" />
+                    </span>
+                    Change Password
+                  </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                  <button
+                    type="button"
+                    className={`nav-link ${tab?.logout ? 'active' : ''}`}
+                    onClick={() => {
+                      setIsLogOut(true);
+                      setTab({
+                        profile: false,
+                        changePassword: false,
+                        logout: true,
+                      });
+                    }}
+                  >
+                    <span>
+                      <img src={logOut} alt="logout-icon" />
+                    </span>
+                    Logout
+                  </button>
+                </li>
+              </ul>
 
-            <div className="tab-content">
-              <div className="tab-pane fade active show">
-                <div className="profile-card">
-                  {isMyAccount ? <MyAccount /> : <ChangePassword />}
+              <div className="tab-content">
+                <div className="tab-pane fade active show">
+                  <div className="profile-card">
+                    {tab?.profile ? (
+                      <MyAccount />
+                    ) : tab?.changePassword ? (
+                      <ChangePassword />
+                    ) : null}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {isLogOut && (
+        <CustomActionModal
+          showModal={isLogOut}
+          closeModal={() => {
+            setIsLogOut(false);
+            setTab({
+              profile: true,
+              changePassword: false,
+              logout: false,
+            });
+          }}
+          message="Do you want to Logout?"
+          onSubmit={() => doLogout()}
+        />
+      )}
+    </>
   );
 };
 
