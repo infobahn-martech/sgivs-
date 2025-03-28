bf0b3f39de52ce65a042b10f417cea1e27f874e1
ba-ba-yaga <SHANID@SPERICORN.COM>
2025-03-28 16:28:28 +0530

    added delete file

--- src/services/inventoryServices.js
+++ src/services/inventoryServices.js
@@ -3,24 +3,14 @@ import Gateway from '../config/gateway';
 const getInventoryListService = (params) =>
   Gateway.get('inventory', { params });
 
-const submitInventoryItems = (formData) =>
-  Gateway.post('inventory', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+const submitInventoryItems = (formData) => Gateway.post('inventory', formData);
 const updateInventoryItems = (formData, itemId) =>
-  Gateway.put(`inventory/${itemId}`, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  Gateway.put(`inventory/${itemId}`, formData);
 
 const getItemByIdService = (itemId) => Gateway.get(`inventory/${itemId}`);
 const deleteItemByIdService = (itemId) => Gateway.delete(`inventory/${itemId}`);
 
-const getBarcode = (itemId) =>
-  Gateway.post(`inventory/barcode`, { itemId });
+const getBarcode = (itemId) => Gateway.post(`inventory/barcode`, { itemId });
 
 export {
   getInventoryListService,
