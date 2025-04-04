e82ddf172ce49b78bcd15f2e045084c83d5e6379
sreelekshmi14 <sreelekshmi@spericorn.com>
2025-04-04 12:36:07 +0530

    bulk-upload-fix

--- src/config/config.js
+++ src/config/config.js
@@ -55,6 +55,12 @@ export const handleDownloadSample = () => {
       'https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
     ],
     ['test 22', '654321', 'test', 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D'],
+    [
+      'sample 4',
+      '123456',
+      '	abc,abcde',
+      'https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+    ],
   ];
 
   const worksheet = XLSX.utils.aoa_to_sheet(sampleData);
