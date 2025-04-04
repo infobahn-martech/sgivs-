641a74c30a2912b85e2a9284f3ac5ba1ec4e1680
Niyas MN <niyas.mn@spericorn.com>
2025-04-04 10:57:03 +0530

    style updates

--- src/components/common/Page404.jsx
+++ src/components/common/Page404.jsx
@@ -3,14 +3,18 @@ import nodataImage from '../../assets/images/404.svg';
 
 const Page404 = () => {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center px-3">
-      <div className="mb-4">
-        <img src={nodataImage} alt="Page Not Found" className="img-fluid" />
+    <div className="no-data not-found">
+      <div className="no-data-content">
+      <div className="no-data-img">
+          <img src={nodataImage} alt="Page Not Found" className="img-fluid" />
       </div>
-      <h2 className="fw-bold mb-2">404 - PAGE NOT FOUND</h2>
-      <p className="text-muted">
-        The page you are looking for is temporarily unavailable.
-      </p>
+      <div className="no-data-txt">
+        <h2 className="">404 - PAGE NOT FOUND</h2>
+        <p className="text-muted">
+          The page you are looking for is temporarily unavailable.
+        </p>
+      </div>
+      </div>
     </div>
   );
 };
