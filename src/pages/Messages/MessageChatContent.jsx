fe8ec2d122ea46e68d86c9c4c1c3c357ab4dbc42
basim km <basimkm555@gmail.com>
2025-04-07 18:06:34 +0530

    message component

--- src/pages/Messages/MessageChatContent.jsx
+++ src/pages/Messages/MessageChatContent.jsx
@@ -1,43 +1,49 @@
-// components/messages/MessageChatContent.js
-import React from 'react';
-import img2 from '../../assets/images/msg-img-2.png';
 import MessageFooter from './MessageFooter';
 
-const MessageChatContent = () => {
+const MessageChatContent = ({
+  selectedContact,
+  messages,
+  message,
+  setMessage,
+  onSend,
+}) => {
   return (
     <div className="message-content-wrap">
       <div className="head-wrap">
         <figure className="img">
-          <img src={img2} alt="" />
+          <img src={selectedContact?.img} alt="" />
         </figure>
         <div className="name-status-wrap">
-          <div className="name">Florencio Dorrance</div>
+          <div className="name">{selectedContact.name}</div>
           <div className="status online">Online</div>
         </div>
       </div>
 
       <div className="body-msg-wrap">
-        <div className="chat-block">
-          <figure className="img">
-            <img src={img2} alt="" />
-          </figure>
-          <div className="chat-content-wrap">
-            <p className="txt">omg, this is amazing</p>
-            <p className="txt">perfect! ✅</p>
-            <p className="txt">Wow, this is really epic</p>
+        {messages?.map((msg, idx) => (
+          <div
+            key={idx}
+            className={`chat-block ${
+              msg.from === 'me' ? 'chat-block-right' : ''
+            }`}
+          >
+            {msg.from === 'them' && (
+              <figure className="img">
+                <img src={selectedContact?.img} alt="" />
+              </figure>
+            )}
+            <div className="chat-content-wrap">
+              <p className="txt">{msg?.text}</p>
+            </div>
           </div>
-        </div>
-
-        <div className="chat-block chat-block-right">
-          <div className="chat-content-wrap">
-            <p className="txt">omg, this is amazing</p>
-            <p className="txt">perfect! ✅</p>
-            <p className="txt">Wow, this is really epic</p>
-          </div>
-        </div>
+        ))}
       </div>
 
-      <MessageFooter />
+      <MessageFooter
+        message={message}
+        setMessage={setMessage}
+        onSend={onSend}
+      />
     </div>
   );
 };
