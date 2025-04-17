2b9286a790301d39fc857fa25258bbc5c599eaa4
HarithaSpericorn <haritha@spericorn.com>
2025-04-17 18:07:44 +0530

    category validation update

--- src/pages/Category/AddEditModal.jsx
+++ src/pages/Category/AddEditModal.jsx
@@ -86,7 +86,7 @@ export function AddEditModal({ showModal, closeModal, onRefreshCategory }) {
                   id="name"
                   className="form-control"
                   autoComplete="off"
-                  maxLength={40}
+                  maxLength={20}
                   {...register('name')}
                 />
                 {errors.name && (
